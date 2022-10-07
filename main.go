4467a5fdad8c7ac28dda803751175e8db64cda0e
Filippo Rivolta <37808229+frivolta@users.noreply.github.com>
2022-10-07 18:32:13 +0200

    adding go routes

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"simplebank/api"
+	"simplebank/db/sqlc"
+	"simplebank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to the db", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Cannot start server", err)
+	}
+}
