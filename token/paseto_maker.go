5eabf671714d1f2af43b2967fa367ad3179bef27
Filippo Rivolta <37808229+frivolta@users.noreply.github.com>
2022-10-10 00:41:17 +0200

    implement paseto maker

---
+++ token/paseto_maker.go
@@ -0,0 +1,47 @@
+package token
+
+import (
+	"fmt"
+	"github.com/o1egl/paseto"
+	"golang.org/x/crypto/chacha20poly1305"
+	"time"
+)
+
+type PasetoMaker struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+func NewPasetoMaker(symmetricKey string) (Maker, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d chars", chacha20poly1305.KeySize)
+	}
+	maker := &PasetoMaker{
+		paseto:       paseto.NewV2(),
+		symmetricKey: []byte(symmetricKey),
+	}
+	return maker, nil
+}
+
+// Create and sign a new token
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+}
+
+// Check if the input is valid or not
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrorInvalidToken
+	}
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
---
+++ token/paseto_maker_test.go
@@ -0,0 +1,65 @@
+package token
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/stretchr/testify/require"
+	"simplebank/util"
+	"testing"
+	"time"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	paylod, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, paylod)
+
+	require.NotZero(t, paylod.ID)
+	require.Equal(t, username, paylod.Username)
+	require.WithinDuration(t, issuedAt, paylod.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, paylod.ExpiraAt, time.Second)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidPasetoTokeAlgNone(t *testing.T) {
+	payload, err := NewPayload(util.RandomOwner(), time.Minute)
+	require.NoError(t, err)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrorInvalidToken.Error())
+	require.Nil(t, payload)
+
+}
